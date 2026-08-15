33175fa12b3443e7cbc17f494353dd852242c10f
Jamie <pomprab.b@gmail.com>
2026-08-15 19:56:48 +0200

    added icon

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $this->e($this->title) ?></title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
